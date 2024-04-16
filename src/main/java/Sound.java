5acd8db33122467d84caf4453c2c95ae4d7814e0
AltonC2 <143027301+AltonC2@users.noreply.github.com>
2024-04-15 20:59:10 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -15,6 +15,15 @@ public class Sound
    */
   public int limitAmplitude(int limit)
   {  
+    int count = 0;
+    for ( int i = 0; i < samples.length;i++){
+      if ( samples[i] > limit)
+        samples[i] = limit;
+    count++;
+    else if ( samples[i]< limit)
+      samples[i] = -limit;
+    count++;
+    }
     /* to be implemented in part (a) */
     return 0;
   }
